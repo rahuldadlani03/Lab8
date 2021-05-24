1fbe621cabb7c56e2b5f97508a9fd41b03c8477d
jonzamora <jonzamora18@gmail.com>
2021-05-23 23:52:46 -0700

    part 2 updates

--- README.md
+++ README.md
@@ -1,23 +1,33 @@
 # Lab8_Starter
+---
+**Partners**: Jonathan Zamora-Anaya, Rahul Dadlani
+
+---
 
 ## Check your understanding q's (FILL OUT)
 1. In your own words: Where would you fit your automated tests in your Bujo project development pipeline? (just write the letter)
 
-**W**
+   - **`A`** `(1) Within a Github action that runs whenever code is pushed `
 
 2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
 
-**I wouldn't since the feature is more than a single unit of code. This feature
-is dependent on a number of other units code like sending the message across a
-network, checking if the message was received, if the message received was
-correct etc.**
+    - **`No`**`, I would not since the feature is more than a single unit of code. This feature is dependent on a number of other units of code like sending the message across a network, checking if the message was received, if the message received was correct etc.`
 
 3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
 
-**Yes. The max message length is a single unit of code and isn't dependent on
-other units of code.**
+    - **`Yes.`**`The max message length is a single unit of code and isn't dependent on other units of code.`
 
 4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
 
+    - `If we run our Puppeteer tests with the field 'headless' set to 'true', then a browser UI will not be shown while we run our tests. Instead, the tests will continue running in the background and the results for those tests will be reported after they have finished running.`
+
 5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
 
+    - If we want to start from the settings page before every test case, we would add "await page.click('header > img');" after "await page.waitForTimeout(500);":
+    ```js
+    beforeAll(async () => {
+        await page.goto('http://127.0.0.1:5500');
+        await page.waitForTimeout(500);
+        await page.click("header > img");
+    });
+    ```
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -29,12 +29,16 @@ describe('Basic user flow for SPA ', () => {
 
   it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
-
+    await page.click("journal-entry");
+    expect(page.url()).toContain("/#entry1");
   });
 
   it('Test4: On first Entry page - checking page header title', async () => {
-    // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
-
+    // implement test4: Clicking on the first journal entry should update the header text to “Entry 1”
+    const header = await page.$eval("header > h1", (entry) => {
+      return entry.innerHTML;
+    });
+    expect(header).toBe("Entry 1");
   });
 
   it('Test5: On first Entry page - checking <entry-page> contents', async () => {
@@ -50,51 +54,100 @@ describe('Basic user flow for SPA ', () => {
           }
         }
       */
+     const entryPage = await page.$("entry-page");
+     const entry = await (await entryPage.getProperty("entry")).jsonValue();
 
+     expect(entry.title).toBe("You like jazz?");
+     expect(entry.date).toBe("4/25/2021");
+     expect(entry.content).toBe(
+       "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.");
+     expect(entry.image.src).toBe("https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455");
+     expect(entry.image.alt).toBe("bee with sunglasses");
   }, 10000);
 
   it('Test6: On first Entry page - checking <body> element classes', async () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
-
+    const bodyClass = await page.$eval("body", (body) => {
+      return body.className;
+    });
+    expect(bodyClass).toBe("single-entry");
   });
 
   it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
     // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
-
+    await page.click("header > img");
+    expect(page.url()).toBe("http://127.0.0.1:5500/#settings");
   });
 
   it('Test8: On Settings page - checking page header title', async () => {
     // implement test8: Clicking on the settings icon should update the header to be “Settings”
-
+    const title = await page.$eval("header > h1", (header) => {
+      return header.innerHTML;
+    });
+    expect(title).toBe("Settings");
   });
 
   it('Test9: On Settings page - checking <body> element classes', async () => {
     // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
-
+    const classSettings = await page.$eval("body", (body) => {
+      return body.className;
+    });
+    expect(classSettings).toBe("settings");
   });
 
   it('Test10: Clicking the back button, new URL should be /#entry1', async() => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
-
+    await page.goBack();
+    expect(page.url()).toContain("/#entry1");
   });
 
   // define and implement test11: Clicking the back button once should bring the user back to the home page
-
+  it('Test 11: Clicking the back button once should bring the user back to the home page', async() => {
+    await page.goBack();
+    expect(page.url()).toBe("http://127.0.0.1:5500/");
+  });
 
   // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
-
+  it('Test 12: When the user if on the homepage, the header title should be “Journal Entries”', async() => {
+    const headerTitle = await page.$eval("header > h1", (heading) => {
+      return heading.innerHTML;
+    });
+    expect(headerTitle).toBe("Journal Entries");
+  });
 
   // define and implement test13: On the home page the <body> element should not have any class attribute 
-
+  it('Test 13: On the home page the <body> element should not have any class attribute', async() => {
+    const bodyClassLength = await page.$eval("body", (bodyElement) => {
+      return bodyElement.classList.length;
+    });
+    expect(bodyClassLength).toBe(0);
+  });
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
-
+  it('Test 14: Verify the url is correct when clicking on the second entry', async() => {
+    await page.click("journal-entry:nth-of-type(2)");
+    expect(page.url()).toContain("/#entry2");
+  });
 
   // define and implement test15: Verify the title is current when clicking on the second entry
-
+  it('Test 15: Verify the title is current when clicking on the second entry', async() => {
+    const headerText = await page.$eval("header > h1", (secondEntry) => {
+      return secondEntry.innerHTML;
+    });
+    expect(secondEntry).toBe("Entry 2");
+  });
 
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
+  it('Test 16: Verify the entry page contents is correct when clicking on the second entry', async() => {
+    const entryPage = await page.$("entry-page");
+    const secondEntry = await (await entryPage.getProperty("entry")).jsonValue();
 
+    expect(secondEntry.title).toBe("Run, Forrest! Run!");
+    expect(secondEntry.date).toBe("4/26/2021");
+    expect(secondEntry.content).toBe("Mama always said life was like a box of chocolates. You never know what you're gonna get.");
+    expect(secondEntry.image.src).toBe("https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg");
+    expect(secondEntry.image.alt).toBe("forrest running");
+  });
 
   // create your own test 17
 
